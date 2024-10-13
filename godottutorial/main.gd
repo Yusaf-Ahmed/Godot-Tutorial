f14ba4e2821708dadb5bc98c8b7029f80e9b0723
Yusaf-Ahmed <150547231+Yusaf-Ahmed@users.noreply.github.com>
2024-10-13 23:24:00 +0100

    If Statements
    Basics, learning that elif and exist in this language. else has to end with a colon for some reason and GScript is not only case sensetive but even gap sensetive, one spacebar could be an error... oof

--- godottutorial/main.gd
+++ godottutorial/main.gd
@@ -3,6 +3,14 @@ extends Node
 var Health = 100
 # Called when the node enters the scene tree for the first time.
 func _input(event):
-	if event.is_action_pressed("Damage"):
+	if event.is_action_pressed("Damage") and Health > 0:
 		Health -= 10
 		print(Health)
+	
+		if Health <= 0:
+			Health = 0
+			print("DEAD")	
+		elif Health < 50:
+			print("INJURED!")	
+		else:
+			print("HEALTHY!")	
