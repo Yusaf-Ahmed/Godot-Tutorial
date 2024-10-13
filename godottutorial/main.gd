97752aa19876f2cdec13c680bfb92e0adecc0265
Yusaf-Ahmed <150547231+Yusaf-Ahmed@users.noreply.github.com>
2024-10-13 23:10:32 +0100

    Variables
    Created Health and Damaged it using SpaceBar. Pretty Basic.

--- godottutorial/main.gd
+++ godottutorial/main.gd
@@ -1,7 +1,8 @@
 extends Node
 
-
+var Health = 100
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	$Label.text = "Hello World!!!"
-	$Label.modulate = Color.PURPLE
+func _input(event):
+	if event.is_action_pressed("Damage"):
+		Health -= 10
+		print(Health)
