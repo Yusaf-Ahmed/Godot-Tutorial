2be9a65ca47d9bd49da3fccab60de39a7d6eddf2
Yusaf-Ahmed <150547231+Yusaf-Ahmed@users.noreply.github.com>
2024-10-16 16:41:41 +0100

    Learning Functions
    Functions, got experience from python code academy which helps me understand this way better than
    I probably would

--- godottutorial/main.gd
+++ godottutorial/main.gd
@@ -6,11 +6,16 @@ func _input(event):
 	if event.is_action_pressed("Damage") and Health > 0:
 		Health -= 10
 		print(Health)
-	
+	# Health System
 		if Health <= 0:
 			Health = 0
-			print("DEAD")	
+			var result = add(2, 800)
+			print("DEAD ", result)	
 		elif Health < 50:
 			print("INJURED!")	
 		else:
 			print("HEALTHY!")	
+
+func add(num1, num2):
+	var result = num1 + num2
+	return result
