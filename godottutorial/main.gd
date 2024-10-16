16abe42a1c83ac985d2704c5c67da76839500e4c
Yusaf-Ahmed <150547231+Yusaf-Ahmed@users.noreply.github.com>
2024-10-16 19:13:07 +0100

    Using randi_range()
    made a random roll out of 1 to 20 and specific statements are printed if you land the required numbers.

--- godottutorial/main.gd
+++ godottutorial/main.gd
@@ -1,9 +1,10 @@
 extends Node
 
-var Health = 100
+var Health = 300
 # Called when the node enters the scene tree for the first time.
 func _input(event):
 	if event.is_action_pressed("Damage") and Health > 0:
+		var roll = randi_range(1, 20)
 		Health -= 10
 		print(Health)
 	# Health System
@@ -15,6 +16,20 @@ func _input(event):
 			print("INJURED!")	
 		else:
 			print("HEALTHY!")	
+		
+	# Random Dice System
+		if roll == 10:
+			print("CONGRATS!!! You got EXACTLY half! - ", roll)
+		elif roll >= 10:
+			print("You got More than half! - ", roll)
+			if roll == 20:
+				print("Also...You got... a very Large number! - ", roll)
+		elif roll <= 10:
+			print("You got Less than half! - ", roll)
+			if roll == 1:
+				print("Also...You got... a very Small number! - ", roll)	
+		else:
+			print(roll)	
 
 func add(num1, num2):
 	var result = num1 + num2
