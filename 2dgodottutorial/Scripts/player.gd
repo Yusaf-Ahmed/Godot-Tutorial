ec7fafb1ec8963ff9c9512132330bcf4de46a1a4
Yusaf-Ahmed <150547231+Yusaf-Ahmed@users.noreply.github.com>
2024-10-23 23:07:33 +0100

    TESTING

--- 2dgodottutorial/Scripts/player.gd
+++ 2dgodottutorial/Scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-#this better work!
+#this better work!!!
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
