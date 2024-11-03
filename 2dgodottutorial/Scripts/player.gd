55b78d5dac831d2b46e385b85ec07910763db960
Yusaf-Ahmed <150547231+Yusaf-Ahmed@users.noreply.github.com>
2024-11-03 14:22:19 +0000

    Adding Nodes
    Building Worldmap

--- 2dgodottutorial/Scripts/player.gd
+++ 2dgodottutorial/Scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
 
 #this better work!!!
 func _physics_process(delta: float) -> void:
