360e9546464fc444ccd210a02ed1c5f69c9f1ed4
Yusaf-Ahmed <150547231+Yusaf-Ahmed@users.noreply.github.com>
2024-11-14 23:22:36 +0000

    Death Slow Motion

--- 2dgodottutorial/Scripts/killzones.gd
+++ 2dgodottutorial/Scripts/killzones.gd
@@ -5,9 +5,12 @@ extends Area2D
 #When Player enters KillZones
 func _on_body_entered(body: Node2D):
 	print("Death!")
+	Engine.time_scale = 0.25
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 
 #When timer hits 0s
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
